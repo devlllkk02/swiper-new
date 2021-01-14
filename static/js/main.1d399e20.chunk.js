18d502cfd36c053383a45e00d58ba879f91b32b7
devlllkk02 <devlllkk02@gmail.com>
2021-01-14 20:51:18 +0530

    Updates

---
+++ static/js/main.1d399e20.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpswiper-new"]=this["webpackJsonpswiper-new"]||[]).push([[0],{10:function(e,n,t){},11:function(e,n,t){"use strict";t.r(n);var c=t(0),r=t(1),s=t.n(r),i=t(3),o=t.n(i);t(9),t(10);var p=function(){return Object(c.jsx)("div",{className:"App",children:"Heyy"})};o.a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(p,{})}),document.getElementById("root"))},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.1d399e20.chunk.js.map
